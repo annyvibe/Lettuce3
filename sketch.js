74c16bae656b297ec244d8db1d9dc78fee8fa0d4
annyvibe <161568188+annyvibe@users.noreply.github.com>
2025-03-10 00:55:45 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,6 +39,36 @@ function setup() {
         requestFullScreen();
         fullscreenButton.style.display = 'none';
     });
+
+}
+function onClick() {
+    // feature detect
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === 'granted') {
+                    window.addEventListener('devicemotion', () => { });
+                }
+            })
+            .catch(console.error);
+    } else {
+        // handle regular non iOS 13+ devices
+    }
+}
+
+function onClick() {
+    // feature detect
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+        DeviceOrientationEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === 'granted') {
+                    window.addEventListener('deviceorientation', () => { });
+                }
+            })
+            .catch(console.error);
+    } else {
+        // handle regular non iOS 13+ devices
+    }
 }
 function startVideo() {
     if (lettuceVideo && lettuceVideo.elt.paused) {
