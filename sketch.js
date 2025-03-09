fe4f118cb07c96f1bd23552b3271f25276d7eb1b
annyvibe <161568188+annyvibe@users.noreply.github.com>
2025-03-09 13:16:44 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,32 +4,31 @@ let hands = [];
 let lettuceVideo;
 
 function preload() {
-    handPose = ml5.handPose();
+    handPose = ml5.handPose;
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    let webcam = createCapture(VIDEO);
-    webcam.size(windowWidth, windowHeight);
-    webcam.hide();
+    video = createCapture(VIDEO);
+    video.size(windowWidth, windowHeight);
+    video.hide();
 
-    // Modify video creation
+    handPose = ml5.handPose(video, modelReady);
+    handPose.on("predict", gotHands);
+
     lettuceVideo = createVideo(['assets/Lettuce.mp4']);
     lettuceVideo.size(windowWidth, windowHeight);
     lettuceVideo.hide();
-    lettuceVideo.volume(0); // Start muted
-
-    // Start hand gesture recognition
-    handPose.detectStart(webcam, gotHands);
+    lettuceVideo.volume(0);
 
     let startButton = document.getElementById('startButton');
     startButton.addEventListener('click', function () {
-        startButton.style.display = 'none'; // 立即隐藏按钮
-        startVideo(); // 然后开始视频
+        startButton.style.display = 'none';
+        startVideo();
     });
 
-    // 创建全屏按钮
+
     let fullscreenButton = document.createElement('button');
     fullscreenButton.id = 'fullscreenButton';
     fullscreenButton.textContent = 'Enter Fullscreen';
@@ -40,33 +39,37 @@ function setup() {
 
     fullscreenButton.addEventListener('click', function () {
         requestFullScreen();
-        fullscreenButton.style.display = 'none'; // 点击后隐藏全屏按钮
+        fullscreenButton.style.display = 'none';
     });
+}
+
+function modelReady() {
+    console.log("HandPose model ready!");
 }
 
 function startVideo() {
     if (lettuceVideo && lettuceVideo.elt.paused) {
         lettuceVideo.loop();
-        lettuceVideo.volume(1); // Set volume to 1 (full volume)
+        lettuceVideo.volume(1);
     }
 }
 
 function touchStarted() {
     if (lettuceVideo && lettuceVideo.elt.paused) {
         lettuceVideo.loop();
-        lettuceVideo.volume(1); // Set volume to 1 (full volume)
+        lettuceVideo.volume(1);
     }
-    return false; // Prevent default
+    return false;
 }
 
 function draw() {
     image(lettuceVideo, 0, 0, width, height);
 
     if (hands.length > 0) {
-        let finger = hands[0].index_finger_tip;
-        let thumb = hands[0].thumb_tip;
-        let pinch = dist(finger.x, finger.y, thumb.x, thumb.y);
-        let speed = map(pinch, 100, 700, 0.1, 2);
+        let finger = hands[0].annotations.indexFinger[3];
+        let thumb = hands[0].annotations.thumb[3];
+        let pinch = dist(finger[0], finger[1], thumb[0], thumb[1]);
+        let speed = map(pinch, 10, 200, 0.1, 2);
         console.log(pinch);
         lettuceVideo.speed(speed);
     } else {
@@ -74,21 +77,17 @@ function draw() {
     }
 }
 
-// 处理手势数据
+
 function gotHands(results) {
     hands = results;
 }
 
-// 全屏功能
+
 function requestFullScreen() {
     let elem = document.documentElement;
     if (elem.requestFullscreen) {
         elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { // Firefox
-        elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari
+    } else if (elem.webkitRequestFullscreen) {
         elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { // IE/Edge
-        elem.msRequestFullscreen();
     }
 }
